73227beeaba1c96e7c3d230a1642c8bde1404476
Sergey Sokolov <serjihsklovski@gmail.com>
2018-07-12 02:59:46 +0300

    sgdisk wrapper: 'create_partition', 'create_partition_as_rest'; enums 'SizeUnit', 'PartitionType'; base partitioning process

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 import config_loader
 import sgdisk
+import re
 from argparse import ArgumentParser
+from sgdisk import SizeUnit, PartitionType
 
 
-def do_disk_partitioning(device: str):
+SIZE_POSTFIX_TO_UNIT = {
+    'MB': SizeUnit.MEGABYTES,
+    'GB': SizeUnit.GIGABYTES,
+}
+
+DEFAULT_CONFIG_PATH = './installation.config.json'
+
+
+def _parse_size_and_unit_postfix(size: str) -> tuple:
+    pattern = re.compile(r'^(?P<size_value>\d+)(?P<size_unit>(MB)|(GB))$')
+    matcher = pattern.search(size)
+    if matcher is None:
+        raise Exception('Wrong format of partition size: you must specify the number and the unit postfix.')
+    return int(matcher.group('size_value')), SIZE_POSTFIX_TO_UNIT[matcher.group('size_unit')]
+
+
+def perform_disk_partitioning(device: str, partitions: list):
     sgdisk.set_device(device)
     sgdisk.clear()
     sgdisk.convert_mbr_to_gpt()
+
+    for partition in partitions[:-1]:
+        size, size_unit = _parse_size_and_unit_postfix(partition['size'])
+        sgdisk.create_partition(size, size_unit, PartitionType[partition['type']], partition['name'])
+
+    if len(partitions) > 1:
+        partition = partitions[-1]
+        sgdisk.create_partition_as_rest(PartitionType[partition['type']], partition['name'])
+
     sgdisk.print_table()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-f', '--file', required=True)
+    parser.add_argument('-f', '--file', default=DEFAULT_CONFIG_PATH)
 
     args = parser.parse_args()
 
     config = config_loader.load(args.file)
 
-    do_disk_partitioning(config['device'])
+    perform_disk_partitioning(config['device'], config['partitions'])
--- sgdisk/__init__.py
+++ sgdisk/__init__.py
@@ -1,4 +1,50 @@
 import subprocess
+from enum import Enum, auto
+from functools import lru_cache
+
+
+class SizeUnit(Enum):
+    MEGABYTES = auto()
+    GIGABYTES = auto()
+
+    @classmethod
+    @lru_cache(maxsize=None)
+    def _get_values(cls) -> map:
+        return {
+            cls.MEGABYTES: 'M',
+            cls.GIGABYTES: 'G',
+        }
+
+    @classmethod
+    def get_value(cls, size_unit: 'SizeUnit') -> str:
+        val = cls._get_values().get(size_unit)
+        if val is None:
+            raise Exception('Cannot recognize size unit: {size_unit}'.format(size_unit=size_unit))
+        return val
+
+
+class PartitionType(Enum):
+    EFI = auto()
+    ROOT = auto()
+    SWAP = auto()
+    HOME = auto()
+
+    @classmethod
+    @lru_cache(maxsize=None)
+    def _get_values(cls) -> map:
+        return {
+            cls.EFI: 'ef00',
+            cls.ROOT: '8300',
+            cls.SWAP: '8200',
+            cls.HOME: '8300',
+        }
+
+    @classmethod
+    def get_value(cls, partition_type: 'PartitionType') -> str:
+        val = cls._get_values().get(partition_type)
+        if val is None:
+            raise Exception('Cannot recognize partition type: {partition_type}'.format(partition_type=partition_type))
+        return val
 
 
 _SUCCESS_OUTPUT = 'The operation has completed successfully.\n'
@@ -17,9 +63,9 @@ def _handle_output_print(output: bytes):
 
 def _sgdisk_wrapper(device_getter, output_handler=_handle_output_not_success):
     def wrap1(sgdisk_cmd):
-        def wrap2():
-            output = subprocess.check_output('sgdisk {cmd} {device}'.format(cmd=sgdisk_cmd(), device=device_getter()),
-                                             shell=True)
+        def wrap2(*args, **kwargs):
+            output = subprocess.check_output(
+                'sgdisk {cmd} {device}'.format(cmd=sgdisk_cmd(), device=device_getter(args, kwargs)), shell=True)
             return output_handler(output)
         return wrap2
     return wrap1
@@ -47,3 +93,31 @@ def clear():
 @_sgdisk_wrapper(get_device)
 def convert_mbr_to_gpt():
     return '--mbrtogpt'
+
+
+@_sgdisk_wrapper(get_device)
+def create_partition(size: int, size_unit: SizeUnit, partition_type: PartitionType, name: str):
+    return '--new=0:0:+{size}{size_unit} --typecode=0:{type_code} --change-name=0:"{name}"'.format_map({
+        'size': size,
+        'size_unit': SizeUnit.get_value(size_unit),
+        'type_code': PartitionType.get_value(partition_type),
+        'name': name,
+    })
+
+
+@_sgdisk_wrapper(get_device)
+def create_partition_as_rest(partition_type: PartitionType, name: str):
+    return '--new=0:0:0 --typecode=0:{type_code} --change-name=0:"{name}"'.format_map({
+        'type_code': PartitionType.get_value(partition_type),
+        'name': name,
+    })
+
+
+if __name__ == '__main__':
+    assert SizeUnit.get_value(SizeUnit.MEGABYTES) == 'M', '`SizeUnit.MEGABYTES` should be "M"'
+    assert SizeUnit.get_value(SizeUnit.GIGABYTES) == 'G', '`SuzeUnit.GIGABYTES` should be "G"'
+
+    assert PartitionType.get_value(PartitionType.EFI) == 'ef00', '`PartitionType.EFI` should be "ef00"'
+    assert PartitionType.get_value(PartitionType.ROOT) == '8300', '`PartitionType.ROOT` should be "8300"'
+    assert PartitionType.get_value(PartitionType.SWAP) == '8200', '`PartitionType.SWAP` should be "8200"'
+    assert PartitionType.get_value(PartitionType.HOME) == '8300', '`PartitionType.HOME` should be "8300"'
