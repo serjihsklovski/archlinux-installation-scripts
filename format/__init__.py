a6d1b108a7f485143eea5ec0d2c970c894b2bcd7
Sergey Sokolov <serjihsklovski@gmail.com>
2018-07-13 04:06:41 +0300

    Formatting: [bugfixes] using 'os.system' instead 'subprocess.check_output'; removed logging

--- format/__init__.py
+++ format/__init__.py
@@ -1,33 +1,22 @@
-import subprocess
+import os
 
 
-_file = None
+def _run_command(f):
+    def wrap(*args, **kwargs):
+        os.system(f(*args, **kwargs))
+    return wrap
 
 
-def _run_command(output_handler=None):
-    def wrap1(f):
-        def wrap2(*args, **kwargs):
-            output = subprocess.check_output(f(*args, **kwargs))
-            if output_handler is not None:
-                output_handler(output)
-        return wrap2
-    return wrap1
-
-
-def log(output):
-    print(output, file=_file)
-
-
-@_run_command(output_handler=log)
+@_run_command
 def fat32(device: str, label: str):
     return '/usr/bin/mkfs.fat -F32 "{device}" -n "{label}"'.format(device=device, label=label.upper())
 
 
-@_run_command(output_handler=log)
+@_run_command
 def ext4(device: str, label: str):
     return '/usr/bin/mkfs.ext4 "{device}" --label="{label}"'.format(device=device, label=label)
 
 
-@_run_command(output_handler=log)
+@_run_command
 def swap(device: str, label: str):
     return 'mkswap "{device}" --label="{label}"'.format(device=device, label=label)
