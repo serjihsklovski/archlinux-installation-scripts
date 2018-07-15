21d9cf5fd495058e79f8cc06d6671319611a197d
Sergey Sokolov <serjihsklovski@gmail.com>
2018-07-15 23:29:47 +0300

    Setting mirrors: [bugfixes] using 'cp' command instead of 'copy'.

--- installation/__init__.py
+++ installation/__init__.py
@@ -6,7 +6,7 @@ def set_mirrors(mirrors: list):
     # backup the current mirrorlist
     utc_date_formatted = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H-%M-%S')
 
-    os.system('copy /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.{date}'.format(date=utc_date_formatted))
+    os.system('cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.{date}'.format(date=utc_date_formatted))
 
     os.system('> /etc/pacman.d/mirrorlist')
     for mirror in mirrors:
