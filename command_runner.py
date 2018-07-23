349166239d6949524b90c99e67f7cd856ff9e459
Sergey Sokolov <serjihsklovski@gmail.com>
2018-07-24 02:16:30 +0300

    Class 'CommandRunner' for running commands; 2 runners: 'run_in_system', 'run_handling_output'.

---
+++ command_runner.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+
+class CommandRunner:
+
+    def __init__(self, code, runner):
+        self._code = code
+        self._runner = runner
+
+    def run(self):
+        self._runner(self._code)
+
+
+def run_in_system(code: str):
+    os.system(code)
+
+
+def run_handling_output(code: list, output_handler):
+    output = subprocess.check_output(code)
+    output_handler(output)
